778a96e6f7ec4ea66f1d76ef15bbd077d8d31fce
Ikechukwu james <ikechukwujames@punch.cool>
2022-03-29 09:35:11 +0100

    refactored changes

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import { FooterContainer } from './styled';
+import React from "react";
+import { FooterContainer } from "./styled";
 
 const Footer: React.FC = () => {
   return (
     <FooterContainer>
       <p>
-        Developed By <a href="https://www.ehsanazizi.me">Ehsan Azizi</a>
+        Developed By <a href="https://iykelistic.netlify.app">Ikechukwu</a>
       </p>
     </FooterContainer>
   );
--- src/store/fetchWeather.ts
+++ src/store/fetchWeather.ts
@@ -1,17 +1,24 @@
-import { createAsyncThunk } from '@reduxjs/toolkit';
-import { ExtendedForecastData, WeatherData } from '../api/types';
-import { fetchExtendedForecastData, fetchWeatherData } from '../api/weather';
-import { getNextSevenDays } from '../utils/dateUtils';
-import { kelvinToCelcius } from '../utils/unitConversion';
-import { setIsInitial, setIsLoading } from './reducers/appReducer';
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { ExtendedForecastData, WeatherData } from "../api/types";
+import { fetchExtendedForecastData, fetchWeatherData } from "../api/weather";
+import { getNextSevenDays } from "../utils/dateUtils";
+import { kelvinToCelcius } from "../utils/unitConversion";
+import { setIsInitial, setIsLoading } from "./reducers/appReducer";
 
 export const fetchWeather = createAsyncThunk(
-  'weather/fetchWeather',
-  async (city: string | { lat: number; lng: number }, { dispatch, rejectWithValue, fulfillWithValue }) => {
+  "weather/fetchWeather",
+  async (
+    city: string | { lat: number; lng: number },
+    { dispatch, rejectWithValue, fulfillWithValue }
+  ) => {
     dispatch(setIsLoading(true));
 
     try {
-      const res = await Promise.all([fetchWeatherData(city), fetchExtendedForecastData(city)]);
+      let res = await Promise.all([
+        fetchWeatherData(city),
+        fetchExtendedForecastData(city),
+      ]);
+      console.log(res);
       dispatch(setIsLoading(false));
 
       if (res[0].cod === 200) {
@@ -21,7 +28,7 @@ export const fetchWeather = createAsyncThunk(
       return rejectWithValue(res[0].message);
     } catch {
       dispatch(setIsLoading(false));
-      return rejectWithValue('Error');
+      return rejectWithValue("Error");
     }
   }
 );
@@ -47,7 +54,7 @@ export const transformWeatherData = (
 
   const next7Days = getNextSevenDays();
 
-  res[1].list.forEach((i: any, index: number) => {
+  /*res[1].list.forEach((i: any, index: number) => {
     forecast.push({
       day: next7Days[index],
       temp: {
@@ -59,7 +66,7 @@ export const transformWeatherData = (
         main: i.weather[0].main,
       },
     });
-  });
+  });*/
 
   return {
     weather,
